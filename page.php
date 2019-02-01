dab7d20f69d12427dabc81a36e620a8e69381f07
wasif <wasif@localhost.localdomain>
2019-02-01 19:12:35 +0500

    setup strcuture

---
+++ page.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class="row">
+
+        <div class="col-lg-12">
+            <h1 class="float-left">Ozone</h1>
+
+            <button class="btn btn-primary float-right">Save Cahnges</button>
+        </div>
+
+    </div>
+
+    <div class="row">
+
+    </div>
+</div>
