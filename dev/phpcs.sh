1711bcdbc6355c9416f978a86498e3a213462203
wasif <wasif0332@gmail.com>
2019-02-27 22:03:31 +0500

    create abstract element class

--- dev/phpcs.sh
+++ dev/phpcs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker run --rm -i \
     --volume "/tmp:/tmp" \
+    --volume "$(pwd):/src" \
     -u $(id -u):$(id -g) \
     php_code_sniffer \
     php /usr/local/bin/phpcs "$@"
--- src/Abstracts/Element.php
+++ src/Abstracts/Element.php
@@ -8,8 +8,74 @@
 
 namespace Ozone\ThemeOptions\Abstracts;
 
+use Ozone\ThemeOptions\interfaces\iElement;
 
-abstract class Element
+/**
+ * Class Element
+ * @package Ozone\ThemeOptions\Abstracts
+ */
+abstract class Element implements iElement
 {
+    /**
+     * @var array
+     */
+    protected $attributes;
 
+    /**
+     * @var string
+     */
+    protected $value;
+
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @return array
+     */
+    public function getAttributes(): array
+    {
+        return $this->attributes;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param array $attributes
+     */
+    public function setAttributes(array $attributes): void
+    {
+        $this->attributes = $attributes;
+    }
+
+    /**
+     * @param string $value
+     */
+    public function setValue(string $value): void
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
 }
