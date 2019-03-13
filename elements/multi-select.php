a13632a11ab7290d6e9c87af516e0a975dafad4f
wasif <wasif0332@gmail.com>
2019-03-13 21:47:55 +0500

    setup markup for multi select

--- elements/multi-select.php
+++ elements/multi-select.php
@@ -5,3 +5,26 @@
  * Date: 3/9/19
  * Time: 7:44 PM
  */
+?>
+<div class="dropdown multiselect open">
+    <button class="btn btn-default dropdown-toggle" type="button">
+        <span class="placeholder"></span>
+        <span class="caret"></span>
+    </button>
+    <div class="dropdown-menu action">
+        <ul class="list-unstyled">
+            <li>
+                <div class="checkbox orange">
+                    <label>
+                        <input type="checkbox" />
+                        <i></i>
+                        <span>label</span>
+                    </label>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <button type="button" class="close">
+        <i class="icon icon-close"></i>
+    </button>
+</div>
